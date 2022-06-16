ddedf80997d11c08474fa18339f34ed5ceb0962c
Nate Costello <natecostello@gmail.com>
2022-06-16 22:00:20 +0000

    fixed inheritance

--- wakespeed.py
+++ wakespeed.py
@@ -1,6 +1,6 @@
 from can.listener import Listener
 from instrument_logger import Instrument
 
-class WakespeedMonitor(instrument_logger.Instrument):
+class WakespeedMonitor(Listener, Instrument):
     pass
 
